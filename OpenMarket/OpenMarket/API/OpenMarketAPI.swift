735e32523609db9289a43ca0ac63b29b751e8a5a
Jinwook Huh <gjwlsdnr0115@gmail.com>
2022-01-26 15:38:22 +0900

    refactor: Result Type을 이용하여 OpenMarketAPI 네트워킹 구현

--- OpenMarket/OpenMarket/API/OpenMarketAPI.swift
+++ OpenMarket/OpenMarket/API/OpenMarketAPI.swift
@@ -13,39 +13,69 @@ class OpenMarketAPI {
     
     private init() {}
     
-    func checkHealth(completion: @escaping (NetworkResult<Any>) -> Void) {
+    func checkHealth(completion: @escaping (Result<Bool, NetworkError>) -> Void) {
         let urlString: String = APIConstants.healthCheckerURL
         guard let url: URL = URL(string: urlString) else {
-            completion(.error("Not able to create URL"))
+            completion(.failure(.invalidURL))
             return
         }
         
-        networkService.get(from: url, as: String.self) { networkResult in
-            completion(networkResult)
+        networkService.get(from: url) { result in
+            switch result {
+            case .success(let data):
+                let healthString: String = String(decoding: data, as: UTF8.self)
+                guard healthString == "\"OK\"" else {
+                    completion(.failure(.serverNotHealthy))
+                    return
+                }
+                completion(.success(true))
+            case .failure(let error):
+                completion(.failure(error))
+            }
         }
     }
     
-    func getProductList(pageNo: Int, itemsPerPage: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+    func getProductList(pageNo: Int, itemsPerPage: Int, completion: @escaping (Result<ProductListData, NetworkError>) -> Void) {
         let urlString: String = APIConstants.productLookupURL + "?page_no=\(pageNo)&items_per_page=\(itemsPerPage)"
         guard let url: URL = URL(string: urlString) else {
-            completion(.error("Not able to create URL"))
+            completion(.failure(.invalidURL))
             return
         }
         
-        networkService.get(from: url, as: ProductListData.self) { networkResult in
-            completion(networkResult)
+        networkService.get(from: url) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let productListData: ProductListData = try JSONDecoder().decode(ProductListData.self, from: data)
+                    completion(.success(productListData))
+                } catch {
+                    completion(.failure(.unableToDecodeData))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
         }
     }
     
-    func lookupProductDetail(productId: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+    func lookupProductDetail(productId: Int, completion: @escaping (Result<ProductData, NetworkError>) -> Void) {
         let urlString: String = APIConstants.productLookupURL + "/\(productId)"
         guard let url: URL = URL(string: urlString) else {
-            completion(.error("Not able to create URL"))
+            completion(.failure(.invalidURL))
             return
         }
         
-        networkService.get(from: url, as: ProductData.self) { networkResult in
-            completion(networkResult)
+        networkService.get(from: url) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let productData: ProductData = try JSONDecoder().decode(ProductData.self, from: data)
+                    completion(.success(productData))
+                } catch {
+                    completion(.failure(.unableToDecodeData))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
         }
     }
 }
--- OpenMarket/OpenMarketTests/OpenMarketTests.swift
+++ OpenMarket/OpenMarketTests/OpenMarketTests.swift
@@ -9,51 +9,36 @@ import XCTest
 
 class OpenMarketTests: XCTestCase {
     func test_health_check_api_returns_ok() {
-        OpenMarketAPI.shared.checkHealth { networkResult in
-            switch networkResult {
-            case .success(let healthdata):
-                guard let healthdata = healthdata as? Bool else {
-                    XCTFail("Test fail: API Health Not OK")
-                    return
-                }
-                XCTAssertTrue(healthdata)
-            case .error(let errorMessage):
-                if let errorMessage: String = errorMessage as? String {
-                    print(errorMessage)
-                }
-                XCTFail("Test Fail: Network Error")
+        OpenMarketAPI.shared.checkHealth { result in
+            switch result {
+            case .success(let healthy):
+                XCTAssertTrue(healthy)
+            case .failure(let error):
+                XCTFail(error.rawValue)
             }
         }
         sleep(1)
     }
     
-    func test_get_product_list_api() {
-        OpenMarketAPI.shared.getProductList(pageNo: 1, itemsPerPage: 10) { networkResult in
-            switch networkResult {
+    func test_get_product_list_api() {        
+        OpenMarketAPI.shared.getProductList(pageNo: 1, itemsPerPage: 10) { result in
+            switch result {
             case .success(let productListData):
-                let productList: ProductListData? = productListData as? ProductListData
-                XCTAssertNotNil(productList)
-            case .error(let errorMessage):
-                if let errorMessage: String = errorMessage as? String {
-                    print(errorMessage)
-                }
-                XCTFail("Test fail: Network Error")
+                print(productListData)
+            case .failure(let error):
+                XCTFail(error.rawValue)
             }
         }
         sleep(1)
     }
-    
+
     func test_get_product_detail_api() {
-        OpenMarketAPI.shared.lookupProductDetail(productId: 522) { networkResult in
-            switch networkResult {
+        OpenMarketAPI.shared.lookupProductDetail(productId: 522) { result in
+            switch result {
             case .success(let productData):
-                let productData: ProductData? = productData as? ProductData
-                XCTAssertNotNil(productData)
-            case .error(let errorMessage):
-                if let errorMessage: String = errorMessage as? String {
-                    print(errorMessage)
-                }
-                XCTFail("Test fail: Network Error")
+                print(productData)
+            case .failure(let error):
+                XCTFail(error.rawValue)
             }
         }
         sleep(1)
