cb0c5d257739b1da8d197b8c887d51591e939928
Jinwook Huh <gjwlsdnr0115@gmail.com>
2022-01-27 12:02:24 +0900

    feat: collevtionView 추가

---
+++ OpenMarket/OpenMarket/View/ProductCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProductCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by Jinwook Huh on 2022/01/27.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+}
